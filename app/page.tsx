ab391b883002e3c6352405330864334ab581c50d
Mohammad Talim <talimmohammad116@gmail.com>
2024-02-12 21:52:50 +0530

    Build the basic layout and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import { FaXTwitter } from "react-icons/fa6";
+
 export default function Home() {
-  return <div>Twitter Clone</div>;
+  return (
+    <div className="grid grid-cols-12 min-h-screen">
+      {/* Left sidebar */}
+      <div className="col-span-3 pl-32">
+        {/* Logo */}
+        <div className="text-4xl p-4">
+          <FaXTwitter />
+        </div>
+      </div>
+
+      {/* Home Feed */}
+      <div className="col-span-6 border-r border-l border-gray-600">
+        Home Feed
+      </div>
+
+      {/* Right Sidebar */}
+      <div className="col-span-3">Right Sidebar</div>
+    </div>
+  );
 }
