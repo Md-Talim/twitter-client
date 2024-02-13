95da243ce65791f1746e8dd27277ceb5aab37989
Mohammad Talim <talimmohammad116@gmail.com>
2024-02-13 10:10:18 +0530

    Refactor: move navigation to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import { LeftSidebar } from "@/components/shared";
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -17,7 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="grid grid-cols-12 min-h-screen">
+          <LeftSidebar />
+
+          {/* Home Feed */}
+          <div className="col-span-6 border-r border-l border-gray-600">
+            {children}
+          </div>
+
+          {/* Right Sidebar */}
+          <div className="col-span-3">Right Sidebar</div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import { LeftSidebar } from "@/components/shared";
-
 export default function Home() {
   return (
-    <div className="grid grid-cols-12 min-h-screen">
-      <LeftSidebar />
-
-      {/* Home Feed */}
-      <div className="col-span-6 border-r border-l border-gray-600">
-        Home Feed
-      </div>
-
-      {/* Right Sidebar */}
-      <div className="col-span-3">Right Sidebar</div>
-    </div>
+    <main>
+      <h1>Homepage</h1>
+    </main>
   );
 }
